1524282f97142aef0d9b0659814de004f562fac5
Prajwal <iprajwal179@gmail.com>
2025-01-10 11:06:18 +0530

    Updated JSON file

--- dcrm/wsgi.py
+++ dcrm/wsgi.py
@@ -8,11 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the 'dcrm' project.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcrm.settings')
 
+# Get the WSGI application for the project
 application = get_wsgi_application()
-
-app = application
